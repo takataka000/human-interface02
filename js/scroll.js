3ed943c79e2952aef3460f90f8e6a6196f5cff3f
komatsuzaki <68366074+takataka000@users.noreply.github.com>
2020-07-25 23:58:28 +0900

    resp

--- js/scroll.js
+++ js/scroll.js
@@ -175,21 +175,15 @@ $(document).ready(function () {
     $(".10").addClass("active");
   });
 });
+
 //スムーススクロール
 $(function () {
-  // #で始まるa要素をクリックした場合に処理
   $("a[href^=#]").click(function () {
-    // 移動先を0px調整する。0を30にすると30px下にずらすことができる。
     var adjust = 0;
-    // スクロールの速度（ミリ秒）
     var speed = 100;
-    // アンカーの値取得 リンク先（href）を取得して、hrefという変数に代入
     var href = $(this).attr("href");
-    // 移動先を取得 リンク先(href）のidがある要素を探して、targetに代入
     var target = $(href == "#" || href == "" ? "html" : href);
-    // 移動先を調整 idの要素の位置をoffset()で取得して、positionに代入
     var position = target.offset().top + adjust;
-    // スムーススクロール linear（等速） or swing（変速）
     $("body,html").animate({ scrollTop: position }, speed, "swing");
     return false;
   });
@@ -222,15 +216,22 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-//ハンバーガー
+// ハンバーガー
 $(function () {
   $(".hamburger").click(function () {
     $(this).toggleClass("active");
-
     if ($(this).hasClass("active")) {
-      $(".head").addClass("active");
+      $('#head').addClass("active");
     } else {
-      $(".head").removeClass("active");
+      $('#head').removeClass("active");
+    }
+  });
+  $('a').click(function () {
+    $(".hamburger").toggleClass("active");
+    if ($(".hamburger").hasClass("active")) {
+      $('#head').addClass("active");
+    } else {
+      $('#head').removeClass("active");
     }
   });
 });
